03eb5ab6a1434b5b3ab697b1c57b340b6a42e0ca
Michael Kintscher <michael@kintscher.dev>
2021-07-11 12:14:15 -0700

    Added ApiBase class to represent external APIs.

---
+++ ApiHelper/ApiBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiHelper
+{
+    /// <summary>
+    /// Represents an external API, using the Singleton design pattern.
+    /// Use the Instance property to access.
+    /// </summary>
+    /// <typeparam name="T">The type of ApiBase, which must implement a public parameterless constructor.</typeparam>
+    public class ApiBase<T> : Singleton<T> where T : new()
+    {
+        /// <summary>
+        /// The name of the API.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
